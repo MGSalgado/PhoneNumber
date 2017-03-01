baeb5c13eb4afc871c5ba057e84de8e6d75a0b07
Alumno <Alumno@328-15>
2017-03-01 12:07:30 -0600

    Master

---
+++ src/phone/number/MobilePhone.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phone.number;
+import java.util.*;
+/**
+ *
+ * @author Alumno
+ */
+public class MobilePhone {
+    private ArrayList<Contact> contactos;
+
+    public MobilePhone() {
+        contactos = new ArrayList<Contact>();
+    }
+    
+   
+  public boolean addContact (Contact contacto){
+      if (findContact(contacto.getName())==-1){
+          contactos.add(contacto);
+          return true;
+      }
+      return false;
+  }  
+  
+  public boolean addContact (String name, int number){
+      return addContact(new Contact(name, number));
+  }
+  
+  /*public Contact findContact(String name){
+      for (Contact tempContact : contactos){
+            if(tempContact.getName().equals(name)){
+                return tempContact;
+            }
+      }return null;
+  }*/
+
+  public int findContact(String name){
+      for(int i = 0; i<contactos.size();i++){
+          if(contactos.get(i).getName().equals(name)){
+              return i;
+          }
+      }return -1;
+  }
+  
+  public boolean removeContact(Contact contacto){
+      int pos= findContact(contacto.getName());
+      if(pos== -1){
+          return false;
+      }
+      contactos.remove(pos);
+      return true;
+      }
+  
+  public boolean removeContact(String name, int number){
+      return removeContact(new Contact(name,number));
+  }
+  
+  public boolean removeContact(String name){
+      return removeContact(new Contact(name,123));
+  }
+  
+  public boolean updateContact(Contact contacto){
+      int pos = findContact(contacto.getName());
+      if(pos == -1){
+          return false;
+      }
+      contactos.get(pos).setName(contacto.getName());
+      contactos.get(pos).setNumber(contacto.getNumber());
+      return true;
+  }
+  
+  public boolean updateContact(String name, int number){
+      return updateContact(new Contact(name,number));
+  }
+  
+  public void listContacts(){
+      for (Contact tempContact : contactos){
+          System.out.println(tempContact.toString());
+      }
+  }
+  
+  public String queryContact(String name){
+      int pos = findContact(name);
+      
+      if(pos==-1){
+          return "Contact not found";
+      }else{
+          return contactos.get(pos).toString();
+      }
+  }
+  }
+
